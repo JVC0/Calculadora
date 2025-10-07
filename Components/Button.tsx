3c1f6c279a489d19027306c034284018073861b6
JVCO <joseph.vanegas@bowwe.com>
2025-10-07 15:45:55 +0100

    Calculadora

---
+++ Components/Button.tsx
@@ -0,0 +1,42 @@
+import { Colors } from "@/utils/Colors";
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+const Button = ({
+	title,
+	type,
+	onPress,
+}: {
+	title: string;
+	type: "top" | "right" | "number";
+	onPress: Function
+}) => {
+	return (
+		<TouchableOpacity
+			style={[
+				styles.button,
+				{
+					backgroundColor:
+						type === "top" ? Colors.btnDark : type === "right" ? Colors.btRight : Colors.btnLight,
+				},
+			]}
+			onPress={onPress}
+		>
+			<Text style={{ fontSize: 34, color: type == "number" ? Colors.black : Colors.white }}>
+				{title}
+			</Text>
+		</TouchableOpacity>
+	);
+};
+export default Button;
+const styles = StyleSheet.create({
+	button: {
+		height: 70,
+		width: 70,
+		borderRadius: 10,
+		padding: 0,
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: Colors.btnDark,
+	},
+});
---
+++ utils/Colors.tsx
@@ -0,0 +1,31 @@
+export const Colors = {
+	light: "#FBFBFB",
+	dark: "#0c263b",
+	btnLight: "#F6F5F2",
+	btnDark: "#09122C",
+	btRight: "#7743DB",
+	black: "#171617",
+	white: "#fefeff",
+	gray: "#f2f2f4",
+};
+export const Lightheme = {
+	light: "#FBFBFB",
+	dark: "#0c263b",
+	btnLight: "#F6F5F2",
+	btnDark: "#09122C",
+	btRight: "#7743DB",
+	black: "#171617",
+	white: "#fefeff",
+	gray: "#f2f2f4",
+};
+
+export const Darktheme = {
+	light: "#FBFBFB",
+	dark: "#0c263b",
+	btnLight: "#F6F5F2",
+	btnDark: "#09122C",
+	btRight: "#7743DB",
+	black: "#171617",
+	white: "#fefeff",
+	gray: "#f2f2f4",
+};
